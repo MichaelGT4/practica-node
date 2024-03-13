5bbf367b38be3071821b0406903c3c250db00316
Michael Gonzalez <michael13117764@gmail.com>
2024-03-13 13:12:05 -0400

    crud realizado

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,67 @@
 import express, { Request, Response } from "express";
 
-// import  from 'express';
 const app = express()
 const port = 3000
 
-app.use(express.json()) // for parsing application/json
+app.use(express.json())
+
+app.use('/',  express.static('public'));
 
 type  User = {id: number, username: string; fullname: string };
 
 let idSecuence = 0
 const users :User[] = []
 
+// funcion para ver usuarios
 app.get('/users', (req: Request, res: Response) => {
     res.send(users);
 })
 
-app.get('/users/:username', (req: Request, res: Response) => {
-    const { username } = req.params;
-    const user = users.find((user) => user.username === username)
-    if (!user) return res.status(404).json({statusCode:404, statusValue:'User not found', message: `The user ${username} was not found` })
+// funcion para ver usuarios por ID
+app.get('/users/:id', (req: Request, res: Response) => {
+    const { id } = req.params;
+    const user = users.find((user) => user.id === Number(id) )
+    if (isNaN(Number(id))) {
+        return res.status(400).json({ 
+            error: 'Invalid ID',
+            statusCode:400, 
+            statusValue:'ID is not a Number', 
+            message: `El ID ingresado no es un numero` 
+        })
+    }else if (!user) {
+        return res.status(404).json({
+            statusCode:404, 
+            statusValue:'User not found', 
+            message: `El usuario con el ${id} no fue encontrado` 
+        })
+    }
     res.json(user);
 
 })
 
-// metodo para crear usuarios
+// funcion para crear usuarios
 app.post('/users', (req: Request, res:Response)=>{
     const {username, fullname} = req.body
 
-    idSecuence += 1
     
+    if(!username){
+        return res.status(400).json({
+            statusCode:400, 
+            statusValue:'Bad Request', 
+            message: 'El campo username no puede estar vacio.'
+        })
+    } else if(users.find((user) => user.username === username)){
+        return res.status(400).json(
+            {
+                statusCode:400, 
+                statusValue:'Bad Request', 
+                message: 'El nombre de usuario ya existe.'
+            }
+        )
+    }
+    
+    idSecuence += 1
+
     const newUser = {
         id: idSecuence,
         username,
@@ -39,15 +72,46 @@ app.post('/users', (req: Request, res:Response)=>{
     res.json(newUser)
 });
 
-// actualizar un usuario en especifico
-// app.put("/users/:id", (req:Request,res:Response)=> {
-//     const user = users.find((user) => user.id == req.params.id);
-//     if(!user) return res.status(400).send("The user is not in the database");
+// funcion para actualizar un usuario
+app.put("/users/:id", (req:Request, res:Response)=> {
+    const { id, username, fullname} = req.params;
+    const user = users.find((user) => user.id == Number(id));
+    if (!user) {
+        return res.status(404).json(
+            {
+                statusCode:404, 
+                statusValue:'User not found', 
+                message: `El usuario con el ${id} no fue encontrado` 
+            }
+        )
+    }
+    console.log(user)
+    console.log(username)
+    const updatedUser = {
+        username: user.username,
+        fullname: user.fullname
+    }
+    console.log(updatedUser)
+    res.json(updatedUser);
+});
 
-//     for(let key in req.body){
-//         user[key] = req.body[key];
-//     }
-//     res.send(user);
-// });
+// funcion para eliminar un usuario
+app.delete('/users/:id', (req:Request,res:Response)=>{
+    const { id } = req.params
+    const index = users.findIndex((user) => user.id === Number(id))
+
+    if (index !== -1){
+        users.splice(index,1)
+        res.status(200).send(users)
+    } else{
+        res.status(404).json(
+            {
+                statusCode:404, 
+                statusValue:'User not found', 
+                message: `El usuario con el ${id} no fue encontrado` 
+            }
+        )
+    }
+})
 
 app.listen(port, () => console.log('Server is running on port ' + port))
